ef9e8657a29fe57e69b8b7f511a42db8236c3057
KireiCesi <quentin.lacaillefraise@gmail.com>
2024-09-11 21:46:52 +0200

    Création d'une interface pour la vie et les pièces
    Ajout d'un UI pour la vie du personnage (avec un programme si il prends des dégâts) et le nombre de pièce dont le personnage dispose.

--- Assets/Assests/gfx/Scripts/Character/CharactersInfo.cs
+++ Assets/Assests/gfx/Scripts/Character/CharactersInfo.cs
@@ -1,15 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CharactersInfo : MonoBehaviour
 {
     public int moneyCount = 0;
+    private int maxHealth = 5;
+    public int health = 5;
+
+    [SerializeField] private Transform heartPrefab;
+    [SerializeField] private Transform heartParent;
+    private List<GameObject> heartObj = new List<GameObject>();
+
 
     private GameManager manager;
+    [SerializeField] private TextMeshProUGUI moneyTxt;
 
     private void Start()
     {
-        manager = GameManager.GetInstance();        
+        manager = GameManager.GetInstance();
+
+        InitHealth();
+    }
+
+    private void Update()
+    {
+        moneyTxt.text = " : " + moneyCount;
+    }
+
+    private void InitHealth()
+    {
+        health = maxHealth;
+
+        for (int i = 0; i < maxHealth; i++)
+        {
+            Transform curHeart = Instantiate(heartPrefab);
+            curHeart.SetParent(heartParent);
+            heartObj.Add(curHeart.gameObject);
+        }
+    }
+
+    private void TakeDamage(int _damage)
+    {
+        health -= _damage;
+        //ReAffiche les coeurs
+        for (int i = 0; i < maxHealth; i++)
+        {
+            if (i > health)
+            {
+                heartObj[i].SetActive(true);
+            }
+            else
+            {
+                heartObj[i].SetActive(false);
+            }
+            
+        }
     }
 }
